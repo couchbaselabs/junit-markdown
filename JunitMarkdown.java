df4dfedba16e9d13001c0fac81577adf6808cd97
Dimitris Christodoulou <dimitris.christodoulou@couchbase.com>
2026-07-15 16:15:10 +0100

    Truncate stack trace if more than 8 KiB

--- JunitMarkdown.java
+++ JunitMarkdown.java
@@ -447,7 +447,7 @@ public class JunitMarkdown {
           .append("<summary>Stack trace</summary>\n")
           .append("\n")
           .append("```\n")
-          .append(childText.trim()).append("\n")
+          .append(truncate(childText.trim())).append("\n")
           .append("```\n")
           .append("\n")
           .append("</details>\n");
